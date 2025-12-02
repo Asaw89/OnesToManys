3285914219f77db1049ff165e062a694f83a3bb0
Asaw89 <alan691saw@aol.com>
2025-12-02 18:25:23 -0500

    updated CRUD

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 import sqlite3
+from pydantic import BaseModel #FastAPI uses pydantic models
+from typing import Optional
 
 app = FastAPI()
 
@@ -28,4 +30,122 @@ def get_albums():
     results = cursor.fetchall()
     connection.close()
     return{"albums":results}
+
+@app.get("/musicians/{musician_id}")
+def get_musician(musician_id: int):
+    connection=sqlite3.connect("music.db")
+    cursor=connection.cursor()
+    cursor.execute("SELECT * FROM musicians WHERE id = ?", (musician_id,))
+    results = cursor.fetchone()
+    connection.close()
+    return{"musician":results}
+
+@app.get("/albums/{album_id}")
+def get_album(album_id:int):
+    connection=sqlite3.connect("music.db")
+    cursor=connection.cursor()
+    cursor.execute("SELECT * FROM albums WHERE id = ?", (album_id,))
+    results = cursor.fetchone()
+    connection.close()
+    return{"album":results}
+
+
+class MusicianCreate(BaseModel):
+    musician_name: str
+    genre: str
+    year_formed: int
+    origin: str
+
+class AlbumCreate(BaseModel):
+    musician_id: int
+    title: str
+    number_of_tracks: int
+    label: str
+    description:str
+
+class MusicianUpdate(BaseModel):
+    musician_name: str
+    genre: Optional[str] = None
+    year_formed: Optional[int] = None
+    origin: Optional[str] = None
+
+class AlbumUpdate(BaseModel):
+    musician_id: int
+    title: str
+    number_of_tracks: int
+    label: str
+    description: str
+
+
+@app.post("/musicians")
+def add_musician(musician: MusicianCreate):
+    connection = sqlite3.connect("music.db")
+    cursor=connection.cursor()
+    cursor.execute(
+        "Insert Into musicians(musician_name,genre,year_formed,origin) VALUES(?, ?, ? ,?)",
+        (musician.musician_name,musician.genre,musician.year_formed,musician.origin)
+    )
+    connection.commit()
+    connection.close()
+    return{"Musician successfully added"}
+
+@app.post("/albums")
+def add_album(album: AlbumCreate):
+    connection = sqlite3.connect("music.db")
+    cursor=connection.cursor()
+    cursor.execute(
+        "Insert Into albums(musician_id,title,number_of_tracks,label,description) VALUES(?, ?, ? ,?,?)",
+        (album.musician_id,album.title,album.number_of_tracks,album.label,album.description)
+    )
+    connection.commit()
+    connection.close()
+    return{"Album successfully added"}
+
+@app.delete("/albums/{album_id}")
+def delete_album(album_id:int):
+    connection = sqlite3.connect("music.db")
+    cursor=connection.cursor()
+    cursor.execute("DELETE FROM albums WHERE id = ?",(album_id,))
+    connection.commit()
+    connection.close()
+    return{"Album successfully deleted"}
+
+@app.delete("/musicians/{musician_id}")
+def delete_musician(musician_id: int):
+    connection=sqlite3.connect("music.db")
+    cursor=connection.cursor()
+    cursor.execute("DELETE FROM musicians WHERE id = ?",(musician_id,))
+    connection.commit()
+    connection.close()
+    return{"Musician successfully deleted"}
+
+@app.put("/musicians/{musician_id}")
+def update_musician(musician_id: int, data: MusicianUpdate):
+    connection = sqlite3.connect("music.db")
+    cursor = connection.cursor()
+    cursor.execute("""
+        UPDATE musicians
+        SET musician_name = ?, genre = ?, year_formed = ?, origin = ?
+        WHERE id = ?
+    """, (data.musician_name, data.genre, data.year_formed, data.origin, musician_id))
+    connection.commit()
+    cursor.execute("SELECT * FROM musicians WHERE id = ?", (musician_id,))
+    updated = cursor.fetchone()
+    connection.close()
+    return {"musician successfully": updated}
+
+@app.put("/albums/{album_id}")
+def update_album(album_id: int, data: AlbumUpdate):
+    connection = sqlite3.connect("music.db")
+    cursor = connection.cursor()
+    cursor.execute("""
+        UPDATE albums
+        SET musician_id = ?, title = ?, number_of_tracks = ?, label = ?, description = ?
+        WHERE id = ?
+    """, (data.musician_id, data.title, data.number_of_tracks, data.label, data.description,album_id))
+    connection.commit()
+    cursor.execute("SELECT * FROM musicians WHERE id = ?", (album_id,))
+    updated = cursor.fetchone()
+    connection.close()
+    return {"album successfully": updated}
 
